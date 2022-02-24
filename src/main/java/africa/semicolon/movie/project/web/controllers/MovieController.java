10ee91f6b6f34ad80e8663153deb2ebae4bb5140
Ozioma <oziomaokoroafor@gmail.com>
2022-02-24 22:45:35 +0100

    Movie controller

--- src/main/java/africa/semicolon/movie/project/web/controllers/MovieController.java
+++ src/main/java/africa/semicolon/movie/project/web/controllers/MovieController.java
@@ -1,2 +1,22 @@
-package africa.semicolon.movie.project.web.controllers;public class MovieController {
+package africa.semicolon.movie.project.web.controllers;
+
+import africa.semicolon.movie.project.data.dtos.MovieDto;
+import africa.semicolon.movie.project.service.movie.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/movie/")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+    @GetMapping("movie")
+    public ResponseEntity<?> getMovieById(Long id){
+        MovieDto savedMovie = movieService.getMovieById(id);
+        return ResponseEntity.ok().body(savedMovie.getTitle());
+    }
 }
