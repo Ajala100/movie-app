0cccb73303c8525df968bd037ca79a0adc14963b
Ozioma <oziomaokoroafor@gmail.com>
2022-02-24 22:45:04 +0100

    Exceptions class

--- src/main/java/africa/semicolon/movie/project/web/exceptions/MovieDoesNotExist.java
+++ src/main/java/africa/semicolon/movie/project/web/exceptions/MovieDoesNotExist.java
@@ -1,2 +1,7 @@
-package africa.semicolon.movie.project.web.exceptions;public class MovieDoesNotExist {
+package africa.semicolon.movie.project.web.exceptions;
+
+public class MovieDoesNotExist extends BusinessLogicException{
+    public MovieDoesNotExist (String message){
+        super(message);
+    }
 }
