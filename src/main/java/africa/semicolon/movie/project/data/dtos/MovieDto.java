8427a43c60c3daceb4f4f1432b0d21bad387a87b
Ozioma <oziomaokoroafor@gmail.com>
2022-02-24 22:48:03 +0100

    Movie dto commit

--- src/main/java/africa/semicolon/movie/project/data/dtos/MovieDto.java
+++ src/main/java/africa/semicolon/movie/project/data/dtos/MovieDto.java
@@ -1,2 +1,24 @@
-package africa.semicolon.movie.project.data.dtos;public class MovieDto {
+package africa.semicolon.movie.project.data.dtos;
+import africa.semicolon.movie.project.data.models.Genre;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class MovieDto {
+    private Long id;
+    private Genre genre;
+    @CreationTimestamp
+    private LocalDate yearReleased;
+    private LocalDateTime duration;
+    private String posterPath;
+    private String title;
+    private Boolean adult;
+    private String mediaType;
+    private String overview;
+
+
 }
