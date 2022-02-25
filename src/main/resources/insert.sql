3f9640b87bfe92101cdcd2e836cea097250cc24e
Ozioma <oziomaokoroafor@gmail.com>
2022-02-25 12:23:51 +0100

    Insert.sql commit

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,4 @@
+insert into movie(id, adult, duration, title)
+values(400, false, 116, "Things to Do in Denver When You're Dead"),
+      (550, false, 139, "Fight Club"),
+      (500, false, 99, "Reservoir Dogs");
