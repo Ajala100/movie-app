ba457b7e973526cc69c6f51e85b9dfff2b0c9015
Ozioma <oziomaokoroafor@gmail.com>
2022-02-25 03:25:12 +0100

    Subscription enum

--- src/main/java/africa/semicolon/movie/project/data/models/Subscription.java
+++ src/main/java/africa/semicolon/movie/project/data/models/Subscription.java
@@ -2,11 +2,13 @@ package africa.semicolon.movie.project.data.models;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity
 public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
