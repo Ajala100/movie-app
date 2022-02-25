4e317722e8f610ae24db6dd1572f98befb63e5a4
Ozioma <oziomaokoroafor@gmail.com>
2022-02-25 03:24:49 +0100

    Movie repository and User model

--- src/main/java/africa/semicolon/movie/project/data/repostories/MovieRepository.java
+++ src/main/java/africa/semicolon/movie/project/data/repostories/MovieRepository.java
@@ -4,4 +4,5 @@ import africa.semicolon.movie.project.data.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
 }
