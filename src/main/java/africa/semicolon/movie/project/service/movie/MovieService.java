1a85a585427d2a432729c4b6f447194314bb4934
Ozioma <oziomaokoroafor@gmail.com>
2022-02-25 03:23:57 +0100

    Movie service commit

--- src/main/java/africa/semicolon/movie/project/service/movie/MovieService.java
+++ src/main/java/africa/semicolon/movie/project/service/movie/MovieService.java
@@ -2,14 +2,16 @@ package africa.semicolon.movie.project.service.movie;
 
 import africa.semicolon.movie.project.data.dtos.MovieDto;
 import africa.semicolon.movie.project.data.models.Movie;
+import africa.semicolon.movie.project.web.exceptions.BusinessLogicException;
+import africa.semicolon.movie.project.web.exceptions.MovieDoesNotExistException;
 
 import java.util.List;
 
 public interface MovieService {
-    MovieDto getMovieById(Long id);
-    List<Movie> searchForMoviesByTitle(String title);
+    MovieDto getMovieById(Long id)throws MovieDoesNotExistException;
+    List<Movie> searchForMoviesByTitle(String title) throws BusinessLogicException;
     List<Movie> getFavoriteMovies();
     void addMovieToFavorite(MovieDto movieDto);
-    void getAllMovies();
+    List<Movie> getAllMovies() throws BusinessLogicException;
 
 }
