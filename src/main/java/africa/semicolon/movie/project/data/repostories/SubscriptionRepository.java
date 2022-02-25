be1ab7d17114b891ab19e302869b9a8a6f2d2113
ajala ahmad <ahmadajala98@gmail.com>
2022-02-25 12:07:36 +0100

    Repositories commit

--- src/main/java/africa/semicolon/movie/project/data/repostories/SubscriptionRepository.java
+++
@@ -1,6 +0,0 @@
-package africa.semicolon.movie.project.data.repostories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
